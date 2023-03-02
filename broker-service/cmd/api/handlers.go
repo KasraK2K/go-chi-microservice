a4ff1d68924699b695f1e15fc94bb49035a1e537
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-02 09:13:19 +0330

    add logger request to broker

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +44,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
@@ -49,11 +57,14 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
 
 	// Call service
-	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	authServiceUrl := "http://authentication-service/authenticate"
+	request, err := http.NewRequest("POST", authServiceUrl, bytes.NewBuffer(jsonData))
 	if err != nil {
 		app.errorJSON(w, err)
 		return
 	}
+
+	request.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	response, err := client.Do(request)
@@ -90,6 +101,42 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		Error:   false,
 		Message: "Authenticated!",
 		Data:    jsonFromService.Data,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	// Create some json we'll send to the auth microservice
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceUrl := "http://logger-service/log"
+	request, err := http.NewRequest("POST", logServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Make sure we get back correct status code
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling log service"))
+		return
+	}
+
+	// Create variable we'll read response.Body into
+	var payload = jsonResponse{
+		Error:   false,
+		Message: "logged",
 	}
 
 	app.writeJSON(w, http.StatusAccepted, payload)
