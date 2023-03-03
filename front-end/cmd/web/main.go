a012b52493f73bdfd94656fc404b4f4b9e88de1f
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-03 16:59:34 +0330

    some changes in front-end and add  mail btn

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -7,13 +7,15 @@ import (
 	"net/http"
 )
 
+const webPort = "80"
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		render(w, "test.page.gohtml")
+		render(w, "test.page.html")
 	})
 
 	fmt.Println("Starting front end service on port 80")
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", webPort), nil)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -22,13 +24,13 @@ func main() {
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+		"./templates/base.layout.html",
+		"./templates/header.partial.html",
+		"./templates/footer.partial.html",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("./templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
--- front-end/front-end-service.dockerfile
+++ front-end/front-end-service.dockerfile
@@ -3,5 +3,6 @@ FROM alpine:latest
 RUN mkdir /app
 
 COPY frontApp /app
+COPY templates /templates
 
 CMD [ "/app/frontApp" ]
